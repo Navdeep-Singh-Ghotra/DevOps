d6ad2bbcc25eacd2d1c62ff0ff93fa2fad0f5d82
navdeep-singh-ghotra <NavdeepSingh.Ghotra@gmail.com>
2018-08-29 07:45:34 -0700

    Updated with SCM and dist repo

---
+++ src/main/java/com/example/devops/demo/proj/DevopsDemoProjApplication.java
@@ -0,0 +1,12 @@
+package com.example.devops.demo.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsDemoProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsDemoProjApplication.class, args);
+	}
+}
